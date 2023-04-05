c0fabae715584273691d678139b592c0c9511841
Fauve-mce <cindy.mareels@outlook.be>
2023-04-05 16:46:14 +0200

    création mobile-new

--- script_motivation.js
+++ script_motivation.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('navLinks')
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
+    navLinks.classList.toggle('mobile-new')
 })
